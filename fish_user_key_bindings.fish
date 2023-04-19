af3fd0db1595927aff2621417f781c189f568673
Tyler <tylerdback.etc@proton.me>
2023-04-19 16:47:57 -0400

    Added keybind support for accepting only next word in autocompletion with Meta-f

--- fish_user_key_bindings.fish
+++ fish_user_key_bindings.fish
@@ -1,12 +1,16 @@
 # User keybindings enabled by default in fish shell. 
-# ( Call function in .config/fish/config.fish ) 
+# ( Call function in ~/.config/fish/config.fish ) 
 
 function fish_user_key_bindings
   # Sets vi keybinds
   fish_vi_key_bindings              
 
-  # Enables using C-f to accept autosuggestions with vi bindings
+  #  Enables using C-f and M-f to accept autosuggestions and next word with 
+  #+ vi bindings.
   for mode in insert default visual 
-    bind -M $mode \cf forward-char  
+    #  replace with forward-char to only accept autosuggestions at the end 
+    #+ of a word.
+    bind -M $mode \cf accept-autosuggestion  
+    bind -M $mode \ef forward-word
   end
 end
